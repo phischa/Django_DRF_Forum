7547c192abf9fd24703cf753f2ba3ecd74b97132
phischa <ph.git@mailbox.org>
2025-05-07 15:49:31 +0200

    first test

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,7 +1,11 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
+from rest_framework import status
 
 
 class LikeTests(APITestCase):
 
-    def test_create_account(self):
-        pass    
+    def test_get_like(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
