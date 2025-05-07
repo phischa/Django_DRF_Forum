1ced97214eb0e27ec3de7e4e7c854088842c3c6f
phischa <ph.git@mailbox.org>
2025-05-07 17:19:56 +0200

    setUp for test

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,6 +1,10 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from .models import Question
+from .api.serializers import QuestionSerializer
+from django.contrib.auth.models import User
+
 
 
 class LikeTests(APITestCase):
@@ -9,3 +13,14 @@ class LikeTests(APITestCase):
         url = reverse('like-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class QuestionTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
+        pass
+
+
+    def test_detail_question(self):
+        pass
