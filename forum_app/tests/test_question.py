7a52047ae8e5c311d166d0481a45a449cb002ff0
phischa <ph.git@mailbox.org>
2025-05-07 19:47:06 +0200

    tests folder

--- forum_app/tests/test_question.py
+++ forum_app/tests/test_question.py
@@ -1,9 +1,10 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-from .models import Question
-from .api.serializers import QuestionSerializer
+from rest_framework.authtoken.models import Token 
 from django.contrib.auth.models import User
+from forum_app.models import Question
+from forum_app.api.serializers import QuestionSerializer
 
 
 
@@ -19,8 +20,23 @@ class QuestionTest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="testuser", password="testpassword")
         self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
+        # self.client = APIClient()
+        # self.client.login(username="testuser", password="testpassword")
+
+        self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
-        self.client.login(username="testuser", password="testpassword")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key )
+
+
+    def test_list_post_question(self):
+        url = reverse('question-list')
+        data= {'title':'Question1',
+                'content':'Content1',
+                'author':self.user.id,
+                'category':'frontend'}
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_detail_question(self):
         url = reverse('question-detail', kwargs={'pk': self.question.id})
@@ -33,14 +49,3 @@ class QuestionTest(APITestCase):
 
         self.assertEqual(Question.objects.count(), 1)
         self.assertEqual(Question.objects.get().author, self.user)
-
-
-    # def test_list_post_question(self):
-    #     url = reverse('question-list')
-    #     data= {'title':'Question1',
-    #             'content':'Content1',
-    #             'author':self.user.id,
-    #             'category':'frontend'}
-        
-    #     response = self.client.post(url, data, format="json")
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
