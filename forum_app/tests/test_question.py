8f1f4c060c64df6e4f7ee639da29792a15a36e3d
phischa <ph.git@mailbox.org>
2025-05-08 10:43:31 +0200

    post test like

--- forum_app/tests/test_question.py
+++ forum_app/tests/test_question.py
@@ -10,18 +10,37 @@ from forum_app.api.serializers import QuestionSerializer
 
 class LikeTests(APITestCase):
 
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
+        # self.client = APIClient()
+        # self.client.login(username="testuser", password="testpassword")
+
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key )
+
     def test_get_like(self):
         url = reverse('like-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_like(self):
+        url = reverse('like-list')
+        data = {'user':self.user.id,
+                'question':self.question.id,
+                'created_at': '02.05.2025'}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 class QuestionTest(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create_user(username="testuser", password="testpassword")
         self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
-        # self.client = APIClient()
-        # self.client.login(username="testuser", password="testpassword")
+        self.client = APIClient()
+        self.client.login(username="testuser", password="testpassword")
 
         self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
