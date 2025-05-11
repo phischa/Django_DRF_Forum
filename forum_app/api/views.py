86d8c42fec45ca2c6719f474c81b356d147e38ad
phischa <ph.git@mailbox.org>
2025-05-11 17:36:36 +0200

    search 6 ordering fields

--- forum_app/api/views.py
+++ forum_app/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from forum_app.models import Like, Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer, LikeSerializer
 from .permissions import IsOwnerOrAdmin, CustomQuestionPermission
@@ -8,7 +9,7 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [CustomQuestionPermission]
-    throttle_classes = [QuestionThrottle]
+    # throttle_classes = [QuestionThrottle]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -23,30 +24,35 @@ class QuestionViewSet(viewsets.ModelViewSet):
         return []
 
 
-
 class AnswerListCreateView(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author__username', 'content']
+    search_fields = ['content'] #'^author__username'; https://www.django-rest-framework.org/api-guide/filtering/#api-guide
+    ordering_fields = ['content', 'author__username']
+    ordering = ['-author__username']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def get_queryset(self):
-        queryset = Answer.objects.all()
+    # def get_queryset(self):
+    #     queryset = Answer.objects.all()
 
-        """ Query Paramenter: Filter können zusammengefügt werden und ermöglichen so eine genauer Suchanfrage
-        z.B. author: bob & content contains: 'Django'.
-        URL: http://127.0.0.1:8000/api/forum/answers/?content=Django&author=bob  """
-        content_param = self.request.query_params.get('content', None)
-        if content_param is not None:
-            queryset = queryset.filter(content__icontains=content_param)
+    #     """ Query Paramenter: Filter können zusammengefügt werden und ermöglichen so eine genauer Suchanfrage
+    #     z.B. author: bob & content contains: 'Django'.
+    #     URL: http://127.0.0.1:8000/api/forum/answers/?content=Django&author=bob  """
+    #     content_param = self.request.query_params.get('content', None)
+    #     if content_param is not None:
+    #         queryset = queryset.filter(content__icontains=content_param)
 
-        username_param = self.request.query_params.get('author', None)
-        if username_param is not None:
-            queryset = queryset.filter(author__username=username_param)
+    #     username_param = self.request.query_params.get('author', None)
+    #     if username_param is not None:
+    #         queryset = queryset.filter(author__username=username_param)
 
-        return queryset
+    #     return queryset
+
 
 class AnswerDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Answer.objects.all()
